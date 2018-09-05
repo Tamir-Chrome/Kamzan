b6c980446f66eb3eed56f6f3f3cd3c765d6c507f
Cherniak Tamir <tamircher123@gmail.com>
2018-09-05 15:58:33 +0300

    remove act in redux

--- app/reducers/actList.js
+++ app/reducers/actList.js
@@ -1,12 +1,11 @@
-
 const actList = (state = [], action) => {
   switch (action.type) {
     case 'ADD_ACT':
       return [...state, [action.id, action.item]];
     case 'REMOVE_ACT': {
-      // params: action.id
-      const newActList = { ...state };
-      delete newActList[action.id];
+      // deep copy
+      const newActList = JSON.parse(JSON.stringify(state));
+      newActList.splice(action.indexOfAct);
       return newActList;
     }
     default:
